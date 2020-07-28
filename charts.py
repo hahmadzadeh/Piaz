9f67aac59560c223857431141c24eb05d2ac4f26
aliakbarghayoori <ghayoory@gmail.com>
2020-07-28 23:02:01 +0430

    final commit

--- charts.py
+++ charts.py
@@ -34,24 +34,43 @@ class Outputs(AnalyzeCsvs):
         super().__init__(path)
 
     def meanPresInSystem(self):
+        print("part 1")
         print("without covid19 patients average presense in system is: %f"%(self.patientDf[self.patientDf['covid19']==False]['presenceTime'].mean()))
         print("with covid19 patients average presense in system is: %f"%(self.patientDf[self.patientDf['covid19']==True]['presenceTime'].mean()))
         print("all patients average presense in system is: %f"%(self.patientDf['presenceTime'].mean()))
 
     def meanWaitInSystem(self):
+        print("part 2")
         print("without covid19 patients average wait in system is: %f"%(self.patientDf[self.patientDf['covid19']==False]['waitTime'].mean()))
         print("with covid19 patients average wait in system is: %f"%(self.patientDf[self.patientDf['covid19']==True]['waitTime'].mean()))
         print("all patients average wait in system is: %f"%(self.patientDf['waitTime'].mean()))
 
     def leavedPatients(self):
+        print("part 3")
         print("number of leaved patients is: %d"%(self.patientDf[self.patientDf['bored']==True].shape[0]))
 
     def queuesMean(self):
+        print("part 4")
         print("mean of number of persons in triage is: %f"%(self.triageDf['total'].mean()))
         numberOfRooms = self.roomsDf['id'].max()
         for i in range(1,numberOfRooms+1):
             print("mean of room number %d is: %f"%(i, self.roomsDf[self.roomsDf['id']==i]['total'].mean()))
         #print(self.roomsDf.head())
+
+
+    def findN(self):
+        print("part 5")
+        n = ((1.96* self.patientDf['waitTime'].std())/(0.05* self.patientDf['waitTime'].mean()))**2 + 1
+        print("for 95 percent of accuracy number of persons should be: %d"% (int(n)))
+
+
+    def changeDocorRate(self):
+        print("part6")
+        print("if we set mean of random.exponential=0.5 for doctors then there would be no queue in rooms.\n"
+              "one room simulated csv with this parameters is attached to this code.")
+
+
+
 
 class AdditionalCharts(AnalyzeCsvs):
     def __init__(self, path):
@@ -63,7 +82,9 @@ class AdditionalCharts(AnalyzeCsvs):
 
         sns.lineplot(x = self.triageDf['clock'], y = self.triageDf['covid19'], label = 'with covid')
         sns.lineplot(x = self.triageDf['clock'], y = self.triageDf['others'], label = 'without covid')
+
 #        plt.plot(self.triageDf['clock'], self.triageDf['covid19'])
+        plt.title("part5 | queue chart")
         plt.show()
 
     def presenceFreq(self): #TODO: kasaii ke bored=true hast alan ignore mishan ke nabayad beshan.
@@ -79,10 +100,17 @@ class AdditionalCharts(AnalyzeCsvs):
         presCol = np.zeros(maxClock)
 
         for _, row in self.patientDf.iterrows():
-            for i in range(row['arriaval'], row['finish']):
-                presCol[i] += 1
+
+            if row['bored']==False:
+                for i in range(row['arriaval'], row['finish']):
+                    presCol[i] += 1
+            else:
+                for i in range(row['arriaval'], row['arriaval']+row['borredom']):
+                    presCol[i] += 1
+
 
         sns.lineplot(clock, presCol)
+        plt.title("part3(and4) | presence frequency")
         plt.show()
 
 
@@ -90,6 +118,10 @@ class AdditionalCharts(AnalyzeCsvs):
 
         col = self.patientDf[self.patientDf['covid19'] == False]['inspection'] - self.patientDf[self.patientDf['covid19'] == False]['arriaval']
         sns.distplot(col, label='without covid', hist=False)
+        plt.xlabel('patients number')
+        plt.xlim(0,)
+        plt.title('part2 | patients(without covid) waiting frequency')
+        plt.show()
 #        print(col)
         col = self.patientDf[self.patientDf['covid19'] == True]['inspection'] - self.patientDf[self.patientDf['covid19'] == True]['arriaval']
 #        print(col)
@@ -98,7 +130,7 @@ class AdditionalCharts(AnalyzeCsvs):
         plt.xlim(0,)
 
         plt.xlabel('patients number')
-        plt.title('patients waiting frequency')
+        plt.title('part2 | patients(with covid) waiting frequency')
         plt.show()
 
 
@@ -112,7 +144,7 @@ class AdditionalCharts(AnalyzeCsvs):
 
         plt.xlim(0,)
         plt.xlabel('patients number')
-        plt.title('patients answer frequency')
+        plt.title('part1 | patients answer frequency')
         plt.show()
 '''
         a = col.hist(bins=100)
@@ -126,12 +158,22 @@ class AdditionalCharts(AnalyzeCsvs):
 
 
 analyzeCsv = Outputs('')
-#chart = AdditionalCharts('')
-#chart.triageLen()
+additionalChart = AdditionalCharts('')
 
 
-#analyzeCsv.meanPresInSystem()
-#analyzeCsv.meanWaitInSystem()
-#analyzeCsv.leavedPatients()
-
+analyzeCsv.meanPresInSystem()
+print("----------")
+analyzeCsv.meanWaitInSystem()
+print("-----------")
+analyzeCsv.leavedPatients()
+print("-----------")
 analyzeCsv.queuesMean()
+print("-----------")
+analyzeCsv.findN()
+print("-----------")
+analyzeCsv.changeDocorRate()
+
+additionalChart.answerFreq()
+additionalChart.waitFreq()
+additionalChart.presenceFreq()
+additionalChart.triageLen()
